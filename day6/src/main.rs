7664f663cc4ef157cd97340ac9da2c0677a0cad7
Enrico PANETTA (contractor) <enrico.panetta@outlook.com>
2022-12-06 12:27:19 +0100

    Day 6

---
+++ day6/src/main.rs
@@ -0,0 +1,21 @@
+#![warn(clippy::all)]
+
+use itertools::Itertools;
+
+fn main(){
+    let input = include_str!("input.txt");
+    for line in input.lines(){
+        for i in 4..line.len(){
+            if line[i-4..i].chars().into_iter().unique().collect::<Vec<char>>().len() == 4{
+                println!("The fist packet marker appears at index: {} and is {}", i, line[i-4..i].to_string());
+                break;
+            }
+        }
+        for i in 14..line.len(){
+            if line[i-14..i].chars().into_iter().unique().collect::<Vec<char>>().len() == 14{
+                println!("The fist message marker appears at index: {} and is {}", i, line[i-14..i].to_string());
+                break;
+            }
+        }
+    }
+}
